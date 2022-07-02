4e48ce910ca7cb8ef2a84d5a570033b290d48eaa
Kinnari Bhalerao <kinbhalerao5@gmail.com>
2022-07-03 00:22:17 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-
+import Image from './components/Image'
 
 function App() {
   return (
     <div className="App">
      <h1 className = "New">Astroverse</h1>
      <p>Galaxies are beautiful!</p>
+     <img src = {require('./images/galaxies.jpg')}/>
+     
     </div>
   );
 }
