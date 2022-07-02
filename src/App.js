afcba1dd869307afdce8ee716083508090884937
Kinnari Bhalerao <kinbhalerao5@gmail.com>
2022-07-03 00:04:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className = "New">Astroverse</h1>
+     <p>Galaxies are beautiful!</p>
+    </div>
+  );
+}
+
+export default App;
